ee6dfa6c9e3354e8ab8446713b962749cbb3bfaf
kszed <82227471+kszed@users.noreply.github.com>
2022-03-05 11:55:04 -0500

    set psi range in constructor

--- src/main/java/frc/robot/subsystems/CompressorSubsystem.java
+++ src/main/java/frc/robot/subsystems/CompressorSubsystem.java
@@ -1,1 +1,15 @@
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class CompressorSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  
+  private Compressor compressor = new Compressor (PneumaticsModuleType.CTREPCM);
+  
+  public CompressorSubsystem() {
+    compressor.enableHybrid(70, 80);
+  }
+  
